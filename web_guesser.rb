46d13856812c83e5d4fbbfd01671eb063cbc0444
lujanfernaud <lujanfernaud@gmail.com>
2017-07-26 22:46:22 +0100

    Implement game logic

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
 
-RANDOM_NUMBER = rand(100)
+RANDOM_NUMBER = rand(100).to_s
 
 get "/" do
-  "The secret number is " + RANDOM_NUMBER.to_s
+  guess = params["guess"]
+
+  message = guess ? message_when_there_is_a(guess) : message_before_guessing
+
+  erb :index, locals: { message: message, number: RANDOM_NUMBER }
+end
+
+def message_when_there_is_a(guess)
+  case guess <=> RANDOM_NUMBER
+  when -1 then "The number is too low!"
+  when  0 then "CORRECT! The secret number is #{RANDOM_NUMBER}."
+  when  1 then "The number is too high!"
+  end
+end
+
+def message_before_guessing
+  "Please guess a number from 0 to 100."
 end
